69fdc5d660aaffae92ff239d3f3b5c66ecb9274a
tav0dev <tav0dev1337+gh@gmail.com>
2025-10-16 17:13:11 -0300

    inicia engramify

---
+++ README.md
@@ -0,0 +1,3 @@
+# clone-tabnews
+
+Implementação do https://tabnews.com.br para o https://curso.dev
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+  return <h1>Seja bem-vindo ao Campo Justo!</h1>;
+}
+
+export default Home;
